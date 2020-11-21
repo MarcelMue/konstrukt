e82a299e940d4b7888622764aa4eaa9f1351e76e
Bricktop <marcel.mueller1@rwth-aachen.de>
2020-11-21 15:36:41 +0100

    Add fallingdaggers pattern (#17)

---
+++ cmd/gen/fallingdaggers/command_test.go
@@ -0,0 +1,88 @@
+package fallingdaggers
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/giantswarm/micrologger"
+)
+
+func Test_command(t *testing.T) {
+	testCases := []struct {
+		name     string
+		c1       string
+		c2       string
+		filename string
+		height   int
+		width    int
+	}{
+		{
+			name:     "case 0: default pattern",
+			filename: "fallingdaggers",
+		},
+		{
+			name:     "case 1: in color pattern",
+			filename: "fallingdaggers-in",
+			c1:       "#e55039",
+			c2:       "#f39c12",
+		},
+		{
+			name:     "case 2: banner resize",
+			filename: "fallingdaggers-wide",
+			width:    2000,
+			height:   400,
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Log(tc.name)
+
+			var err error
+
+			var logger micrologger.Logger
+			{
+				c := micrologger.Config{}
+
+				logger, err = micrologger.New(c)
+				if err != nil {
+					t.Fatal("expected", nil, "got", err)
+				}
+			}
+
+			c := Config{
+				Logger: logger,
+			}
+
+			testCommand, err := New(c)
+			if err != nil {
+				t.Fatal("expected", nil, "got", err)
+			}
+
+			args := []string{}
+			if tc.filename != "" {
+				args = append(args, []string{"--filename", fmt.Sprintf("../../../samples/%s.svg", tc.filename)}...)
+			}
+			if tc.c1 != "" {
+				args = append(args, []string{"--color1", tc.c1}...)
+			}
+			if tc.c2 != "" {
+				args = append(args, []string{"--color2", tc.c2}...)
+			}
+			if tc.height != 0 {
+				args = append(args, []string{"--height", strconv.Itoa(tc.height)}...)
+			}
+			if tc.width != 0 {
+				args = append(args, []string{"--width", strconv.Itoa(tc.width)}...)
+			}
+
+			testCommand.SetArgs(args)
+
+			err = testCommand.Execute()
+			if err != nil {
+				t.Fatal("expected", nil, "got", err)
+			}
+		})
+	}
+}
